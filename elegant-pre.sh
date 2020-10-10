62738e34b80f6e71be8be4267cb69e61d6884057
guolg <guolg@cn.ibm.com>
2020-10-10 15:52:00 +0800

    install git

--- elegant-pre.sh
+++ elegant-pre.sh
@@ -23,6 +23,7 @@ elif [ "$os" = "Red Hat" ]; then
     echo "This is redhat os."
     sudo yum install zsh -y
     sudo yum install vim -y
+    sudo yum install git -y
     git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
     cp ~/.zshrc ~/.zshrc.bak
     cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
